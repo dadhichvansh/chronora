a3264be9bafb423112714731a8de301f40beffd8
vansh-home <dadhichvansh46@gmail.com>
2025-09-07 23:53:40 +0530

    Update MongoDB connection string

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,18 +1,16 @@
-import mongoose from "mongoose";
-
-// MongoDB connection string from environment variables
-const connectionString = `${process.env.MONGO_URI}/${process.env.DB_NAME}`;
+import mongoose from 'mongoose';
+import { MONGO_CONNECTION_STRING } from '../constants.js';
 
 // Function to connect to MongoDB
 export const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(connectionString);
+    const connectionInstance = await mongoose.connect(MONGO_CONNECTION_STRING);
     console.log(
-      "MongoDB connected successfully:",
+      'MongoDB connected successfully:',
       connectionInstance.connection.host
     );
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error('Error connecting to MongoDB:', error);
     process.exit(1); // Exit process with failure
   }
 };
