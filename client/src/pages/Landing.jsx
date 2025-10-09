6d916690d1a4368da8f05a64a502ff5b1450ca2d
vansh-home <dadhichvansh46@gmail.com>
2025-10-09 18:22:53 +0530

    Create Landing page UI

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import { FeaturesSection } from '../components/FeaturesSection';
+import { HeroSection } from '../components/HeroSection';
+import { TestimonialsSection } from '../components/TestimonialsSection';
+
+export function Landing() {
+  return (
+    <>
+      <HeroSection />
+      <FeaturesSection />
+      <TestimonialsSection />
+    </>
+  );
+}
