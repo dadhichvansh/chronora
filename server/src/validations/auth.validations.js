eb349955b7dff52dcc27f2e716b1fef28511bbb3
vansh-home <dadhichvansh46@gmail.com>
2025-10-18 19:38:45 +0530

    Update user login/register validation schemas

--- server/src/validations/auth.validations.js
+++ server/src/validations/auth.validations.js
@@ -1,18 +1,29 @@
 import z from 'zod';
 
-export const UserRegistrationSchema = z.object({
-  username: z.string().min(3, 'Username must be at least 3 characters long'),
-  email: z.email(),
-  password: z
-    .string()
-    .min(6, 'Password must be at least 6 characters long')
-    .max(30, 'Password must be at most 30 characters long'),
-});
+/**
+ * Zod validation schemas
+ */
+
+const usernameSchema = z
+  .string()
+  .min(3, 'Username must be at least 3 characters long')
+  .max(30, 'Username must be at most 30 characters long')
+  .lowercase('Username must be in lowercase');
+
+const passwordSchema = z
+  .string()
+  .min(6, 'Password must be at least 6 characters long')
+  .max(30, 'Password must be at most 30 characters long');
+
+/**
+ * Schema for user login/registration validation
+ */
 
 export const UserLoginSchema = z.object({
   email: z.email(),
-  password: z
-    .string()
-    .min(6, 'Password must be at least 6 characters long')
-    .max(30, 'Password must be at most 30 characters long'),
+  password: passwordSchema,
+});
+
+export const UserRegistrationSchema = UserLoginSchema.extend({
+  username: usernameSchema,
 });
