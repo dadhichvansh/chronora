7def2579e8335c9571804c0e27bbbb010f96f609
vansh-home <dadhichvansh46@gmail.com>
2025-10-19 01:27:00 +0530

    Define user logout route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { loginUser, registerUser } from '../controllers/auth.controller.js';
+import { loginUser, logoutUser, registerUser } from '../controllers/auth.controller.js';
 
 const router = Router();
 
 // User routes
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/logout', logoutUser);
 
 export default router;
