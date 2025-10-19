d768d25a04d985c464c1504b39908fdf53cedf57
vansh-home <dadhichvansh46@gmail.com>
2025-10-20 02:05:44 +0530

    Update: Add auth-based routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,29 @@ import { ErrorPage } from './pages/ErrorPage';
 import { Home } from './pages/Home';
 import { Landing } from './pages/Landing';
 import { Auth } from './pages/Auth';
-
-const isLoggedIn = false;
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    children: [{ path: '/', element: isLoggedIn ? <Home /> : <Landing /> }],
-  },
-  { path: '/auth', element: <Auth /> },
-]);
+import { useAuth } from './contexts/AuthContext';
+import { RedirectIfAuth } from './components/RedirectIfAuth';
 
 function App() {
+  const { user } = useAuth();
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <RootLayout />,
+      errorElement: <ErrorPage />,
+      children: [{ path: '/', element: user ? <Home /> : <Landing /> }],
+    },
+    {
+      path: '/auth',
+      element: (
+        <RedirectIfAuth>
+          <Auth />
+        </RedirectIfAuth>
+      ),
+    },
+  ]);
+
   return <RouterProvider router={router} />;
 }
 
