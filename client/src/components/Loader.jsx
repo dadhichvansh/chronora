66e76175920094811e7088fbbf212d5895efb1c8
vansh-home <dadhichvansh46@gmail.com>
2025-11-04 18:32:43 +0530

    Update loader component exports

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,4 +1,4 @@
-const Loader = () => {
+export function Loader() {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-background/80 backdrop-blur-sm">
       <div className="flex flex-col items-center gap-4">
@@ -12,6 +12,4 @@ const Loader = () => {
       </div>
     </div>
   );
-};
-
-export default Loader;
+}
