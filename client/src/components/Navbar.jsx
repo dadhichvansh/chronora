ccf7205d5cb38259fcb83be15fe0cd6ea5c18cea
vansh-home <dadhichvansh46@gmail.com>
2025-11-05 19:26:28 +0530

    Update user's name on the avatar dropdown menu in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -109,7 +109,7 @@ export function Navbar() {
                   <DropdownMenuLabel className="font-normal">
                     <div className="flex flex-col space-y-1">
                       <p className="text-sm font-medium leading-none">
-                        {user.email?.split('@')[0]}
+                        {user.username || user.email?.split('@')[0]}
                       </p>
                       <p className="text-xs leading-none text-muted-foreground">
                         {user.email}
