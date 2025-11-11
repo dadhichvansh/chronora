7463eb459b0b95999e580c8ac6a3edb1ab0b2ff4
vansh-home <dadhichvansh46@gmail.com>
2025-11-11 20:17:24 +0530

    Create API for blog posts

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import requestIp from 'request-ip';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
+import postRoutes from './routes/post.routes.js';
 import { authMiddleware } from './middlewares/auth.middleware.js';
 
 // Initialize express app
@@ -35,5 +36,6 @@ app.get('/', (req, res) => {
 
 app.use(authMiddleware); // Apply auth middleware to all routes below
 app.use('/api/auth', authRoutes); // Auth routes
+app.use('/api/posts', postRoutes); // Post routes
 
 export default app;
