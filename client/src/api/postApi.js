b59312c6ed02efb56eb826b55eb3d45496f11546
vansh-home <dadhichvansh46@gmail.com>
2025-12-02 20:03:45 +0530

    Define methods for blog post API calls at client-side

---
+++ client/src/api/postApi.js
@@ -0,0 +1,9 @@
+import api from '../lib/axios';
+
+export const postApi = {
+  createPost: (data) => api.post('/api/posts', data),
+  getAllPosts: () => api.get('/api/posts'),
+  getUserPosts: (userId) => api.get(`/api/posts/${userId}`),
+  updatePost: (id, data) => api.put(`/api/posts/${id}`, data),
+  deletePost: (id) => api.delete(`/api/posts/${id}`),
+};
