5387bc4bf8e8051ff8dbdc8a484eeb17a986371d
vansh-home <dadhichvansh46@gmail.com>
2025-12-12 16:13:54 +0530

    Add getPostById function for fetching individual post by its ID

--- client/src/api/postApi.js
+++ client/src/api/postApi.js
@@ -2,6 +2,7 @@ import api from '../lib/axios';
 
 export const postApi = {
   createPost: (data) => api.post('/api/posts', data),
+  getPostById: (postId) => api.get(`/api/posts/${postId}`),
   getAllPosts: () => api.get('/api/posts'),
   getUserPosts: (userId) => api.get(`/api/posts/${userId}`),
   updatePost: (id, data) => api.put(`/api/posts/${id}`, data),
