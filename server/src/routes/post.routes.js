df05dd9d6831f6761fd766aa34ba258721912910
vansh-home <dadhichvansh46@gmail.com>
2025-12-12 21:33:13 +0530

    Update api route for fetching user posts conflicting with update posts api

--- server/src/routes/post.routes.js
+++ server/src/routes/post.routes.js
@@ -16,7 +16,7 @@ router.get('/', getAllPosts);
 
 // Protected routes for creating, updating, and deleting posts
 router.post('/', uploadCoverImage, createPost);
-router.get('/:userId', getUserPosts);
+router.get('/u/:userId', getUserPosts);
 router.get('/:postId', getPostById);
 router.put('/:postId', uploadCoverImage, updatePost);
 router.delete('/:postId', deletePost);
