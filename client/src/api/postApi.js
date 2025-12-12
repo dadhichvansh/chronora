34cbf9f11176d1ad00ab705941230b3b869a5f5c
vansh-home <dadhichvansh46@gmail.com>
2025-12-12 21:33:46 +0530

    Update getUserPosts API endpoint

--- client/src/api/postApi.js
+++ client/src/api/postApi.js
@@ -4,7 +4,7 @@ export const postApi = {
   createPost: (data) => api.post('/api/posts', data),
   getPostById: (postId) => api.get(`/api/posts/${postId}`),
   getAllPosts: () => api.get('/api/posts'),
-  getUserPosts: (userId) => api.get(`/api/posts/${userId}`),
+  getUserPosts: (userId) => api.get(`/api/posts/u/${userId}`),
   updatePost: (id, data) => api.put(`/api/posts/${id}`, data),
   deletePost: (id) => api.delete(`/api/posts/${id}`),
 };
