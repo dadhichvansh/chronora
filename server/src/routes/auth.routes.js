1dcb90ee72977894a4fb11079d1ce9b4e1221964
vansh-home <dadhichvansh46@gmail.com>
2025-12-14 22:54:14 +0530

    Define auth route for changing user password

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,11 +1,17 @@
 import { Router } from 'express';
-import { loginUser, logoutUser, registerUser } from '../controllers/auth.controller.js';
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  changePassword,
+} from '../controllers/auth.controller.js';
 
 const router = Router();
 
-// User routes
+// User auth routes
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
+router.put('/change-password', changePassword);
 
 export default router;
