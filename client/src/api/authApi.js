a713e2b7775bcddaa0ac2f58db1983a0730881a8
vansh-home <dadhichvansh46@gmail.com>
2025-12-14 21:02:16 +0530

    Remove getCurrentUser() api function from authApi

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -4,5 +4,4 @@ export const authApi = {
   register: (data) => api.post('/api/auth/register', data),
   login: (data) => api.post('/api/auth/login', data),
   logout: () => api.post('/api/auth/logout'),
-  getCurrentUser: () => api.get('/api/auth/me'),
 };
