d11110b6e31246abd0e65cbe66be3be651a9a100
vansh-home <dadhichvansh46@gmail.com>
2025-12-14 22:01:01 +0530

    Update: added display name, image field to the User model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -3,8 +3,11 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
+    displayName: { type: String, default: '' },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    image: { type: String, default: '' },
+    imagePublicId: { type: String, default: '' },
     role: { type: String, enum: ['user', 'admin'], default: 'user' },
     isVerified: { type: Boolean, default: false },
     lastLogin: { type: Date },
