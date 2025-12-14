df1c22bf676f6e6247bf6c923980835588b1b2be
vansh-home <dadhichvansh46@gmail.com>
2025-12-14 23:01:02 +0530

    Added changePassword() function to authApi

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -4,4 +4,5 @@ export const authApi = {
   register: (data) => api.post('/api/auth/register', data),
   login: (data) => api.post('/api/auth/login', data),
   logout: () => api.post('/api/auth/logout'),
+  changePassword: (data) => api.put('/api/auth/change-password', data),
 };
