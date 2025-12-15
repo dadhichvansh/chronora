f8fc5ac30e55dfb7975c751a72c4028a8068f1d7
vansh-home <dadhichvansh46@gmail.com>
2025-12-15 17:02:17 +0530

    Define delete route for deleting comments

--- server/src/routes/comment.routes.js
+++ server/src/routes/comment.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { createComment } from '../controllers/comment.controller.js';
+import { createComment, deleteComment } from '../controllers/comment.controller.js';
 
 const router = Router();
 
 router.post('/', createComment);
+router.delete('/:commentId', deleteComment);
 
 export default router;
