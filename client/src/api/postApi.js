f2cbcbca08fb39c085667c2e99664f95da90c997
vansh-home <dadhichvansh46@gmail.com>
2025-12-31 00:23:36 +0530

    Rename getAllPosts -> getFeed

--- client/src/api/postApi.js
+++ client/src/api/postApi.js
@@ -1,9 +1,9 @@
 import api from '../lib/axios';
 
 export const postApi = {
-  createPost: (data) => api.post('/api/posts', data),
+  getFeed: () => api.get('/api/posts'),
   getPostById: (postId) => api.get(`/api/posts/${postId}`),
-  getAllPosts: () => api.get('/api/posts'),
+  createPost: (data) => api.post('/api/posts', data),
   getUserPosts: (userId) => api.get(`/api/posts/u/${userId}`),
   updatePost: (id, data) => api.put(`/api/posts/${id}`, data),
   deletePost: (id) => api.delete(`/api/posts/${id}`),
